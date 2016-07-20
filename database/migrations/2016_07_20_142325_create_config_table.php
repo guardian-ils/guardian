3a2b61825714f6374c9cadd1a7695d1736814eb7
Koala Yeung <koalay@gmail.com>
2016-07-20 22:25:34 +0800

    Initial table definitions

---
+++ database/migrations/2016_07_20_142325_create_config_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configs', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->text('name');
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('configs');
+    }
+}
