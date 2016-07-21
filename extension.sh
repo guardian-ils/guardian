b120076ad8dea37142d2982cd1ff1c01f0c0959c
Tony Yip <tony@opensource.hk>
2016-07-21 17:42:51 +0800

    Fix extension shell script

--- extension.sh
+++ extension.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Install PostgreSQL Extension for HHVM" && \
-apt-get install -y hhvm-dev cmake make g++ && \
+add-apt-repository -y ppa:george-edison55/cmake-3.x && \
+apt-get update && \
+apt-get -y upgrade && \
+apt-get install -y hhvm-dev cmake build-essential && \
+g++ --version && \
+cmake --version && \
 git clone https://github.com/PocketRent/hhvm-pgsql.git && \
 cd hhvm-pgsql && \
 hphpize && \
 cmake -DHACK_FRIENDLY=ON . && \
 make && \
-ln -s /usr/lib/x86_64-linux-gnu/hhvm/extensions/pgsql.so "$(pwd)/pgsql.so" && \
+ln -s "$(pwd)/pgsql.so" /usr/lib/x86_64-linux-gnu/hhvm/extensions/pgsql.so && \
 echo "hhvm.extensions[pgsql] = pgsql.so" >> /etc/hhvm/php.ini
