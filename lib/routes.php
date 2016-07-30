3b66f1fb307055490250013a2c350e64beb7a324
Koala Yeung <koalay@gmail.com>
2016-07-30 20:49:50 +0800

    Use BranchController for Branch API
    Remove Closure in [lib/routes.php](lib/routes.php).
    
    Fix error `Unable to prepare route [api/v1/branches] for
    serialization. Uses Closure.`

--- lib/routes.php
+++ lib/routes.php
@@ -1,21 +1,4 @@
 <?php
 
-use Illuminate\Http\Response;
-
-Route::get('/api/v1/branches', function () {
-    $content = [
-      'result' => 'success',
-      'data' => [],
-    ];
-    return (new Response(json_encode($content), 200))
-                ->header('Content-Type', 'application/json');
-});
-
-Route::post('/api/v1/branches', function () {
-    $content = [
-      'result' => 'success',
-      'data' => [],
-    ];
-    return (new Response(json_encode($content), 201))
-                ->header('Content-Type', 'application/json');
-});
+Route::get('api/v1/branches', 'Guardian\Controllers\BranchController@listing');
+Route::post('/api/v1/branches', 'Guardian\Controllers\BranchController@create');
