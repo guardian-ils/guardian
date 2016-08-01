21423b7a7808c7fb3e5da9bcfc6b25cd81c3fda8
Tony Yip <tony@opensource.hk>
2016-08-01 18:46:16 +0800

    Add Request Validation and implements branch controller

--- lib/Controllers/BranchController.php
+++ lib/Controllers/BranchController.php
@@ -2,28 +2,35 @@
 
 namespace Guardian\Controllers;
 
+use Guardian\Models\Branch;
+use Guardian\Requests\Branch\CreateRequest;
+use Guardian\Requests\Request;
+
 class BranchController extends Controller {
 
     /**
      * @return \Illuminate\Http\JsonResponse
      */
     public function index() {
+        $branches = Branch::all();
         $content = [
             'result' => 'success',
-            'data' => [],
+            'data' => $branches->toJson(),
         ];
         return response()->json($content);
     }
 
-    /**
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store() {
-        $content = [
-            'result' => 'success',
-            'data' => [],
-        ];
-        return response()->json($content)->setStatusCode(201);
+    public function store(CreateRequest $request) {
+        try {
+            $branch = new Branch($request->getForm()->all());
+            $branch->save();
+            $content = ['result' => 'success'];
+            $status = 201;
+        } catch (\Exception $e) {
+            $content = ['result' => 'failed'];
+            $status = 500;
+        }
+        return response()->json($content)->setStatusCode($status);
     }
 
 }
